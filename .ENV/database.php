167c5800f9053eb43e610679582c82472b3d137b
Joshua Nehemia <joshuans224@gmail.com>
2025-10-22 22:00:44 +0700

    cek

--- .ENV/database.php
+++ .ENV/database.php
@@ -1,8 +1,6 @@
 <?php
-// Database Configuration
 define("DATABASE_ADDRESS","localhost");
 define("DATABASE_SCHEMA","fullstack");
 define("DATABASE_USER_ID","root");
 define("DATABASE_USER_PASSWORD","");
-
 ?>
